a02f73922d35755c88564f1bc03832452ed9f2dd
ThanhNhan <tn.nhan98@gmail.com>
2023-11-17 17:57:46 +0900

    create a new user CRUD

--- src/migrations/migration-create-user.js
+++ src/migrations/migration-create-user.js
@@ -29,7 +29,7 @@ module.exports = {
             roleId: {
                 type: Sequelize.STRING,
             },
-            phoneNymber: {
+            phoneNumber: {
                 type: Sequelize.STRING,
             },
             positionId: {
