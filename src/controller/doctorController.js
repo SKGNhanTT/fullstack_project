6b7d6f799be93176f9fccc7fb09cc5235ec6cfe2
ThanhNhan <tn.nhan98@gmail.com>
2023-12-09 14:20:40 +0900

    create api get all doctor, post detail doctor and get detail doctor by id

--- src/controller/doctorController.js
+++ src/controller/doctorController.js
@@ -15,6 +15,48 @@ let getTopDoctorHome = async (req, res) => {
     }
 };
 
+let getAllDoctor = async (req, res) => {
+    try {
+        let doctor = await doctorService.getAllDoctor();
+        return res.status(200).json(doctor);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMess: 'Error from server!',
+        });
+    }
+};
+
+let postInfoDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInfoDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMess: 'Error from server!',
+        });
+    }
+};
+
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let info = await doctorService.getDetailDoctorById(req.query.id);
+        return res.status(200).json(info);
+    } catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            errCode: -1,
+            errMess: 'Error from server!!',
+        });
+    }
+};
+
 module.exports = {
     getTopDoctorHome,
+    getAllDoctor,
+    postInfoDoctor,
+    getDetailDoctorById,
 };
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -35,6 +35,98 @@ let getTopDoctorHome = (limit) => {
     });
 };
 
+let getAllDoctor = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let doctor = await db.User.findAll({
+                where: { roleId: 'R2' },
+                attributes: {
+                    exclude: ['password', 'image'],
+                },
+            });
+
+            resolve({
+                errCode: 0,
+                data: doctor,
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+let saveDetailInfoDoctor = ({ data }) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data.doctorId || !data.contentHTML || !data.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMess: 'Missing parameter!',
+                });
+            } else {
+                await db.Markdown.create({
+                    contentHTML: data.contentHTML,
+                    contentMarkdown: data.contentMarkdown,
+                    description: data.description,
+                    doctorId: data.doctorId,
+                });
+                resolve({
+                    errCode: 0,
+                    errMess: 'Save info doctor success!!',
+                });
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+let getDetailDoctorById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMess: 'Missing required parameter!!',
+                });
+            } else {
+                let data = await db.User.findOne({
+                    where: { id },
+                    attributes: {
+                        exclude: ['password', 'image'],
+                    },
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: [
+                                'description',
+                                'contentHTML',
+                                'contentMarkdown',
+                            ],
+                        },
+                        {
+                            model: db.Allcode,
+                            as: 'positionData',
+                            attributes: ['valueEn', 'valueVi'],
+                        },
+                    ],
+                    raw: true,
+                    nest: true,
+                });
+                resolve({
+                    errCode: 0,
+                    data,
+                });
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
 module.exports = {
     getTopDoctorHome,
+    getAllDoctor,
+    saveDetailInfoDoctor,
+    getDetailDoctorById,
 };
