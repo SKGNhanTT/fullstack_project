26c4aefb0933715cdb57914c6a10c53dc46e6bed
ThanhNhan <tn.nhan98@gmail.com>
2023-12-10 18:37:07 +0900

    edit api saveDetailInfoDoctor create and update informatuon doctor

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -58,18 +58,37 @@ let getAllDoctor = () => {
 let saveDetailInfoDoctor = ({ data }) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.doctorId || !data.contentHTML || !data.contentMarkdown) {
+            if (
+                !data.doctorId ||
+                !data.contentHTML ||
+                !data.contentMarkdown ||
+                !data.action
+            ) {
                 resolve({
                     errCode: 1,
                     errMess: 'Missing parameter!',
                 });
             } else {
-                await db.Markdown.create({
-                    contentHTML: data.contentHTML,
-                    contentMarkdown: data.contentMarkdown,
-                    description: data.description,
-                    doctorId: data.doctorId,
-                });
+                if (data.action === 'CREATE') {
+                    await db.Markdown.create({
+                        contentHTML: data.contentHTML,
+                        contentMarkdown: data.contentMarkdown,
+                        description: data.description,
+                        doctorId: data.doctorId,
+                    });
+                } else if (data.action === 'EDIT') {
+                    let markdown = await db.Markdown.findOne({
+                        where: { doctorId: data.doctorId },
+                        raw: false,
+                    });
+                    if (markdown) {
+                        markdown.contentHTML = data.contentHTML;
+                        markdown.contentMarkdown = data.contentMarkdown;
+                        markdown.description = data.description;
+
+                        await markdown.save();
+                    }
+                }
                 resolve({
                     errCode: 0,
                     errMess: 'Save info doctor success!!',
@@ -93,7 +112,7 @@ let getDetailDoctorById = (id) => {
                 let data = await db.User.findOne({
                     where: { id },
                     attributes: {
-                        exclude: ['password', 'image'],
+                        exclude: ['password'],
                     },
                     include: [
                         {
@@ -110,9 +129,15 @@ let getDetailDoctorById = (id) => {
                             attributes: ['valueEn', 'valueVi'],
                         },
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true,
                 });
+                if (data && data.image) {
+                    data.image = Buffer.from(data.image, 'base64').toString(
+                        'binary'
+                    );
+                }
+
                 resolve({
                     errCode: 0,
                     data,
