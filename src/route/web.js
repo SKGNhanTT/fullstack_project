98054ff1a19f6e12c143c3eaa424d516b78e17df
ThanhNhan <tn.nhan98@gmail.com>
2023-12-23 22:12:46 +0900

    create api getAllClinic and getDetailClinicById

--- src/route/web.js
+++ src/route/web.js
@@ -73,11 +73,11 @@ let initWebRoutes = (app) => {
     );
 
     router.post('/api/create-new-clinic', clinicController.createClinic);
-    // router.get('/api/get-all-specialty', specialtyController.getAllSpeicalty);
-    // router.get(
-    //     '/api/get-detail-specialty-by-id',
-    //     specialtyController.getDetailSpecialtyById
-    // );
+    router.get('/api/get-all-clinic', clinicController.getAllClinic);
+    router.get(
+        '/api/get-detail-clinic-by-id',
+        clinicController.getDetailClinicById
+    );
 
     return app.use('/', router);
 };
--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -35,6 +35,71 @@ let createClinic = (data) => {
     });
 };
 
+let getAllClinic = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await db.Clinic.findAll();
+            if (data && data.length > 0) {
+                data.map((item) => {
+                    item.image = Buffer.from(item.image, 'base64').toString(
+                        'binary'
+                    );
+                    return item;
+                });
+            }
+            resolve({
+                errCode: 0,
+                errMess: 'Ok',
+                data,
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+let getDetailClinicById = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!id) {
+                resolve({
+                    errCode: 1,
+                    errMess: 'Missing required parameter!',
+                });
+            } else {
+                let data = await db.Clinic.findOne({
+                    where: { id },
+                    attributes: [
+                        'descriptionHTML',
+                        'descriptionMarkdown',
+                        'name',
+                        'address',
+                    ],
+                });
+                if (data) {
+                    let doctorClinic = [];
+
+                    doctorClinic = await db.Doctor_Infor.findAll({
+                        where: { clinicId: id },
+                        attributes: ['doctorId'],
+                    });
+
+                    data.doctorClinic = doctorClinic;
+                } else data = {};
+                resolve({
+                    errCode: 0,
+                    errMess: 'Ok',
+                    data,
+                });
+            }
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
 module.exports = {
     createClinic,
+    getAllClinic,
+    getDetailClinicById,
 };
