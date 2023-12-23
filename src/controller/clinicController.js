fced7593801133f96adeef0804c3d5a0a5217f2f
ThanhNhan <tn.nhan98@gmail.com>
2023-12-23 22:12:27 +0900

    create api getAllClinic and getDetailClinicById

--- src/controller/clinicController.js
+++ src/controller/clinicController.js
@@ -12,6 +12,32 @@ let createClinic = async (req, res) => {
     }
 };
 
+let getAllClinic = async (req, res) => {
+    try {
+        let response = await clinicService.getAllClinic();
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: 1,
+            errMess: 'Error from server!',
+        });
+    }
+};
+
+let getDetailClinicById = async (req, res) => {
+    try {
+        let response = await clinicService.getDetailClinicById(req.query.id);
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(200).json({
+            errCode: 1,
+            errMess: 'Error from server!',
+        });
+    }
+};
+
 module.exports = {
     createClinic,
+    getAllClinic,
+    getDetailClinicById,
 };
